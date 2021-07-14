a409b81ccd13a1e1f4146721a944d721177ed746
jsewai <junyasewai@gmail.com>
2021-07-14 00:36:42 -0400

    added boilerplate for swiper

---
+++ drop-down-menu/index.js
@@ -0,0 +1,8 @@
+const menuContainer = document.querySelector(".menu-container");
+const lineContainer = document.querySelector(".line-container");
+
+lineContainer.addEventListener("click", ()=> {
+    lineContainer.classList.toggle("active")
+    menuContainer.classList.toggle("active")
+})
+
